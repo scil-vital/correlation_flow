4247aaad6cbbe45e47c094c04579fa4e0496a354
fdumais <felix.dumais@imeka.ca>
2022-07-07 09:57:33 -0400

    First commit

---
+++ finta_test_retest.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+  echo "$(basename "$0") [-i inputs -t tractometry_flow -r registration_flow -c correlation_flow -s subjects -b bundles_config -o output_dir -n processes -a target_anat]" 1>&2
+  exit 1
+}
+
+while getopts "i:t:r:c:o:s:n:a:b:" args; do
+  case "${args}" in
+  i) inputs=${OPTARG} ;;
+  t) tractometry_flow=${OPTARG} ;;
+  r) registration_flow=${OPTARG} ;;
+  c) correlation_flow=${OPTARG} ;;
+  o) output=${OPTARG} ;;
+  s) subjects=${OPTARG} ;;
+  n) processes=${OPTARG} ;;
+  a) target_anat=${OPTARG} ;;
+  b) bundles_config=${OPTARG} ;;
+  *) usage ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "$inputs" ] ||
+  [ -z "$tractometry_flow" ] ||
+  [ -z "$registration_flow" ] ||
+  [ -z "$correlation_flow" ] ||
+  [ -z "$subjects" ] ||
+  [ -z "$output" ] ||
+  [ -z "$processes" ] ||
+  [ -z "$target_anat" ] ||
+  [ -z "$bundles_config" ]; then
+  usage
+else
+  echo "Inputs: ${inputs}"
+  echo "Tractometry_flow: ${tractometry_flow}"
+  echo "Registration_flow: ${registration_flow}"
+  echo "Correlation_flow: ${correlation_flow}"
+  echo "Subjects: ${subjects}"
+  echo "Bundles_config: ${bundles_config}"
+  echo "Outputs: ${output}"
+  echo "Processes: ${processes}"
+  echo "Target_anat: ${target_anat}"
+fi
+
+tractometry_dir=$output/tractometry_flow
+registration_dir=$output/registration_flow
+correlation_dir=$output/correlation_flow
+
+mkdir -p "$tractometry_dir"
+mkdir -p "$registration_dir"
+mkdir -p "$correlation_dir"
+
+run_tractometryflow() {
+  local suffix=$1
+  local input=$2
+
+  # Run tractometry-flow
+  nextflow run "$tractometry_flow"/main.nf -w "$tractometry_dir"/work_"$suffix" --output_dir "$tractometry_dir"/output_"$suffix" \
+    --input "$input" -with-report "$tractometry_dir"/report_"${suffix}".html --use_provided_centroids false -resume --processes $processes \
+    --min_streamline_count 50000
+}
+
+run_registrationflow() {
+  local suffix=$1
+  local input=$2
+
+  # Run tractometry-flow
+  nextflow run "$registration_flow"/main.nf -w "$registration_dir"/work_"$suffix" --output_dir "$registration_dir"/output_"$suffix" \
+    --input "$input" -resume --processes $processes --target_anat ${target_anat} --resampling -1
+}
+
+run_correlationflow() {
+  local suffix=$1
+  local input=$2
+
+  # Run tractometry-flow
+
+  nextflow run "$correlation_flow"/main.nf -w "$correlation_dir"/work_"$suffix" --output_dir "$correlation_dir"/output_"$suffix" \
+    --input "$input" --subjects_config $subjects --bundles_config $bundles_config -resume --processes $processes
+}
+
+run_tractometryflow "tractometry" "$inputs"
+
+for icc in ICC11 ICC21 ICC31 ICC1k ICC2k ICC3k; do
+  compute_test_retest_stats.py --length_stats "$tractometry_dir"/output_tractometry/Statistics/length_stats.json \
+    --volume_stats "$tractometry_dir"/output_tractometry/Statistics/volumes.json \
+    --streamline_count "$tractometry_dir"/output_tractometry/Statistics/streamline_count.json \
+    --subject $subjects -o $output/test_retest_${icc} -f --icc "${icc}"
+done
+
+run_registrationflow "registration" "$inputs"
+
+sub=$(jq 'keys[]' ${subjects})
+mkdir -p ${registration_dir}/inputs_correlation
+whole_path_registration=$(readlink -f ${registration_dir}/output_registration)
+for s in $sub; do
+  s=$(eval echo $s)
+  mkdir -p ${correlation_dir}/inputs_correlation/${s}
+  ln -sf ${whole_path_registration}/${s}* ${correlation_dir}/inputs_correlation/${s}/
+done
+
+run_correlationflow "correlation" "${correlation_dir}/inputs_correlation"
---
+++ main.nf
@@ -0,0 +1,115 @@
+#!/usr/bin/env nextflow
+
+if(params.help) {
+    usage = file("$baseDir/USAGE")
+    cpu_count = Runtime.runtime.availableProcessors()
+
+    bindings = ["cpu_count":"$cpu_count"]
+
+    engine = new groovy.text.SimpleTemplateEngine()
+    template = engine.createTemplate(usage.text).make(bindings)
+    print template.toString()
+    return
+}
+
+cpu_count = Runtime.runtime.availableProcessors()
+if (!params.processes){
+    processes = cpu_count
+}
+else{
+    processes = params.processes
+}
+
+log.info "Dice Test-Retest"
+log.info "==============================================="
+log.info ""
+log.info "Start time: $workflow.start"
+log.info ""
+
+log.debug "[Command-line]"
+log.debug "$workflow.commandLine"
+log.debug ""
+
+log.info "[Git Info]"
+log.info "$workflow.repository - $workflow.revision [$workflow.commitId]"
+log.info ""
+
+log.info "Options"
+log.info "======="
+log.info "Num Processes: ${processes}"
+log.info ""
+log.info ""
+
+workflow.onComplete {
+    log.info "Pipeline completed at: $workflow.complete"
+    log.info "Execution status: ${ workflow.success ? 'OK' : 'failed' }"
+    log.info "Execution duration: $workflow.duration"
+}
+
+log.info "Input: $params.input"
+root = file(params.input)
+
+Channel
+    .fromFilePairs("$root/**/*/Register_Streamlines/*trk", size: -1) { it.parent.parent.parent.name }
+    .set{ subjects } // [sid, AF.trk, IFOF.trk, ...]
+
+subjects_config = Channel.fromPath("$params.subjects_config")
+Channel.fromPath("$params.bundles_config").into{ bundles_config; bundles_config_for_agreements }
+
+
+
+subjects
+    .combine(subjects_config)
+    .combine(bundles_config)
+    .set{files_for_agreements} 
+
+
+process Compute_Agreements {
+    errorStrategy 'ignore'
+    memory '20 GB'
+
+    input:
+    set sid, file(subjects), file(subjects_config), file(bundles_config) from files_for_agreements
+
+    output:
+    file "*.json" into agreement_results
+
+    script:
+    String bundles_list = subjects.join(", ").replace(',', '')
+    """
+    bundle=\$(jq 'keys[]' ${bundles_config})
+    for b in \$bundle
+    do 
+        b=\$(eval echo \$b)
+        if [[ "${bundles_list}" == *"\${b}"* ]]; then
+            files=\$(echo ${bundles_list} |xargs -d' ' -n 1| grep \$b)
+            scil_evaluate_bundles_pairwise_agreement_measures.py \$files ${sid}_\${b}.json \
+            --indent 4 --sort_keys -f --processes ${processes}
+        fi
+
+    done
+    """
+}
+
+agreement_results
+    .collect()
+    .set{all_files_for_agreements}
+
+process Aggregate_Agreements {
+    publishDir = params.statsPublishDir
+
+    input:
+    file(metrics) from all_files_for_agreements
+    file(config) from bundles_config_for_agreements
+
+    output:
+    file "*.html"
+    file "*.json"
+
+    script:
+    String metrics_list = metrics.join(", ").replace(',', '')
+    """
+    plot_agreements.py ${metrics_list} . --bundles_config ${config}
+    """
+}
+
